e794cb7ec51d6678c4ac23fe3e167ceddef9953f
talalafzal <talalafzal57@gmail.com>
2023-02-17 03:59:06 -0800

    fabonacci numbers

---
+++ test/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func(int) int {
+	a, b := 0, 1
+	return func(x int) int {
+		c := a
+		a, b = b, a+b
+		return c
+
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f(i))
+
+	}
+}
