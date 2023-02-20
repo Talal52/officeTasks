20a65f8d7a730afe56b4a889e6c23efba9dff6e1
talalafzal <talalafzal57@gmail.com>
2023-02-20 04:43:20 -0800

    sqrt function

--- findingSqrt/main.go
+++ findingSqrt/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := float64(1)
+	for i := 1; i < 10; i++ {
+		z = z - (z*z-x)/(2*z)
+
+	}
+	return z
+}
 
 func main() {
-	fmt.Println("testing repo")
-	fmt.Println("again testing")
+	t := 25.0
+	fmt.Println(Sqrt(t))
+	fmt.Println(math.Sqrt(t))
 
 }
