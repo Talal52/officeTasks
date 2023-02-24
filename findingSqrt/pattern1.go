b928b450c41be330fc34cb7481356a485c8b9613
talalafzal <talalafzal57@gmail.com>
2023-02-24 04:22:01 -0800

    star pqttern

--- findingSqrt/pattern1.go
+++ findingSqrt/pattern1.go
@@ -1,22 +1,25 @@
 package main
 
+
 import "fmt"
 
-func main() {
-	fmt.Print("Enter length of triangle: ")
-	var first int
-	fmt.Scanln(&first)
 
-	for i := 0; i < first; i++ {
-		for j := 0; j < i+1; j++ {
-			// for k := 0; k < j+1; k++ {
-			// 	fmt.Print("")
-			// }
-			fmt.Print(" ")
-		}
-			for k :=0 ; k < first; k++ {
-				fmt.Print("* ")
-			}
-			fmt.Println("")
-	}
+func main() {
+   row := 5
+   
+   fmt.Println("\nLeft Angled Star Pattern")
+   
+   for i := 0; i <= row; i++ {
+   
+      
+      for j := 0; j < row-i; j++ {
+         fmt.Print(" ")
+      }
+
+      for k := 0; k < i+1; k++ {
+         fmt.Print("*")
+      }
+      
+      fmt.Println()
+   }
 }
