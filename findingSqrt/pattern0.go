e136752fe779b73c7f54ee123e2833d99c1e5b76
talalafzal <talalafzal57@gmail.com>
2023-02-24 07:17:23 -0800

    star triangle

--- findingSqrt/pattern0.go
+++ findingSqrt/pattern0.go
@@ -1,22 +1,19 @@
 package main
 
-
 import "fmt"
 
-
 func main() {
-   row := 5
-      
-      
-   for i := 0; i <= row; i++ {
-         for j := 0; j < i; j++ {
-            fmt.Print(" ")
-         }
-   
-         for k := 0; k < row-1; k++ {
-            fmt.Print("*")
-         }
-         
-         fmt.Println()
-      }
+	row := 5
+
+	for i := 0; i < row; i++ {
+
+		for j := 0; j < row-i; j++ {
+			fmt.Print("  ")
+		}
+
+		for k := 0; k <= i*2; k++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
 }
